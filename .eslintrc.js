3173493e627db406081051c8df69e652be40575b
nathan <cornelie.nathanpro@gmail.com>
2024-10-06 12:32:19 +0200

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
 	root: true,
 	ignorePatterns: ['static/**/*.js'],
-	env: {
-		browser: true,
-		node: true,
-	},
-	parserOptions: {
-		parser: '@babel/eslint-parser',
-		requireConfigFile: false,
-	},
-	extends: ['@nuxtjs', '@hakatashi'],
-	plugins: ['prettier'],
-	rules: {
-		'node/no-unsupported-features': 'off',
-		'node/no-unsupported-features/es-syntax': 'off',
-		semi: ['error', 'always'],
-		'import/order': 'off',
-		'import/extensions': 'off',
-		// destroyed was introduced in Vue v3
-		'vue/no-deprecated-destroyed-lifecycle': 'off',
-	},
-	settings: {
-		'import/resolver': 'nuxt',
-	},
-};
+	// env: {
+	// 	browser: true,
+	// 	node: true,
+	// },
+	// parserOptions: {
+	// 	parser: '@babel/eslint-parser',
+	// 	requireConfigFile: false,
+	// },
+	// extends: ['@nuxtjs', '@hakatashi'],
+	// plugins: ['prettier'],
+	// rules: {
+	// 	'node/no-unsupported-features': 'off',
+	// 	'node/no-unsupported-features/es-syntax': 'off',
+	// 	semi: ['error', 'always'],
+	// 	'import/order': 'off',
+	// 	'import/extensions': 'off',
+	// 	// destroyed was introduced in Vue v3
+	// 	'vue/no-deprecated-destroyed-lifecycle': 'off',
+	// },
+	// settings: {
+	// 	'import/resolver': 'nuxt',
+	// },
+}
