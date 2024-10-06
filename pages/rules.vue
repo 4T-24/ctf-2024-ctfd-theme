c0c77a32693358c785276d19d53e7ce8acd38d45
nathan <cornelie.nathanpro@gmail.com>
2024-10-06 12:56:08 +0200

    [~] rules style

--- pages/rules.vue
+++ pages/rules.vue
@@ -9,51 +9,95 @@
 		<div>
 			<p>Welcome to the rules page for our event !</p>
 
-			<p>To ensure a fair and enjoyable experience for everyone, please take a moment to read through the competition guidelines.</p>
-
-			<h1> Team </h1>
-			<ul>
-				<li> Each team is limited to a maximum of 4 members. </li>
-				<li> All members must register an account to be eligible for prizes. </li>
-				<li> Each participant can only be part of one team throughout the event. </li>
-				<li> Collaboration with other teams or third parties is strictly prohibited. </li>
-				<li> Teams must choose appropriate names. Offensive or inappropriate names are not allowed, they will be renamed or deleted. </li>
-				<li> Teams will accumulate points as a group, individual performance will not affect the team's overall score. </li>
-			</ul>
-
-			<h1> Infrastructure </h1>
-			<ul>
-				<li> Participants are prohibited from attacking the CTF platform, website, or any competition-related infrastructure (e.g., scoreboard, servers). Any attempt to disrupt or modify the infrastructure will result in disqualification. </li>
-			</ul>
-
-			<h1> Flag </h1>
-			<ul>
-				<li> Flags will follow the following format : 4T${} (e.g., 4T${example}). Only flags in the correct format will be accepted. </li>
-				<li> Sharing flags with other teams or outside individuals is strictly prohibited. </li>
-				<li> Do not attempt to brute force flag submissions. Excessive incorrect submissions may lead to penalties or disqualification. </li>
-				<li> Dynamic Scoring: The points for a challenge decrease over time as more teams solve it. </li>
-			</ul>
-
-			<h1> Scoring </h1>
-			<ul>
-				<li> The scoreboard will update in real-time, reflecting each team's current score based on their flag submissions. </li>
-				<li> Once the competition ends, the final scoreboard will determine team rankings. </li>
-				<li> In case of a tie, the team with earliest flag submissions will be ranked higher. </li>
-			</ul>
-
-			<h1> Communication </h1>
-			<ul>
-				<li>  Use the designated official communication platforms (Discord) for any announcements, queries, or issue reporting. Avoid using unofficial channels for event-related discussions. </li>
-			</ul>
-
-			<h1> Acknowledgment </h1>
 			<p>
-				By participating in this Capture the Flag (CTF) competition, all participants and teams agree to abide by the rules and guidelines outlined on this page. Failure to follow these rules may result in penalties, including disqualification from the event.
+				To ensure a fair and enjoyable experience for everyone, please take a
+				moment to read through the competition guidelines.
 			</p>
 
+			<h1>Team</h1>
+			<ul>
+				<p>Each team is limited to a maximum of 4 members.</p>
+				<p>All members must register an account to be eligible for prizes.</p>
+				<p>
+					Each participant can only be part of one team throughout the event.
+				</p>
+				<p>
+					Collaboration with other teams or third parties is strictly
+					prohibited.
+				</p>
+				<p>
+					Teams must choose appropriate names. Offensive or inappropriate names
+					are not allowed, they will be renamed or deleted.
+				</p>
+				<p>
+					Teams will accumulate points as a group, individual performance will
+					not affect the team's overall score.
+				</p>
+			</ul>
+
+			<h1>Infrastructure</h1>
+			<ul>
+				<p>
+					Participants are prohibited from attacking the CTF platform, website,
+					or any competition-related infrastructure (e.g., scoreboard, servers).
+					Any attempt to disrupt or modify the infrastructure will result in
+					disqualification.
+				</p>
+			</ul>
+
+			<h1>Flag</h1>
+			<ul>
+				<p>
+					Flags will follow the following format : 4T${} (e.g., 4T${example}).
+					Only flags in the correct format will be accepted.
+				</p>
+				<p>
+					Sharing flags with other teams or outside individuals is strictly
+					prohibited.
+				</p>
+				<p>
+					Do not attempt to brute force flag submissions. Excessive incorrect
+					submissions may lead to penalties or disqualification.
+				</p>
+				<p>
+					Dynamic Scoring: The points for a challenge decrease over time as more
+					teams solve it.
+				</p>
+			</ul>
+
+			<h1>Scoring</h1>
+			<ul>
+				<p>
+					The scoreboard will update in real-time, reflecting each team's
+					current score based on their flag submissions.
+				</p>
+				<p>
+					Once the competition ends, the final scoreboard will determine team
+					rankings.
+				</p>
+				<p>
+					In case of a tie, the team with earliest flag submissions will be
+					ranked higher.
+				</p>
+			</ul>
+
+			<h1>Communication</h1>
+			<ul>
+				<p>
+					Use the designated official communication platforms (Discord) for any
+					announcements, queries, or issue reporting. Avoid using unofficial
+					channels for event-related discussions.
+				</p>
+			</ul>
+
+			<h1>Acknowledgment</h1>
+			<p>
+				By participating in this Capture the Flag (CTF) competition, all
+				participants and teams agree to abide by the rules and guidelines
+				outlined on this page. Failure to follow these rules may result in
+				penalties, including disqualification from the event.
+			</p>
 		</div>
-
-
 
 		<!-- eslint-enable vue/no-v-html -->
 	</section>
@@ -117,12 +161,9 @@ export default {
 		margin: 0 auto;
 		font-size: 1.4rem;
 		line-height: 2rem;
-	}
-	& > div {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		justify-content: center;
 		font-weight: 600;
 		text-align: center;
 	}
